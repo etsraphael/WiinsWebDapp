8183ccda6c15675d2b96a9c8b4b4e30599e9caa3
etsraphael <ets_raphael@outlook.fr>
2022-02-28 19:59:48 +1000

    before to add the first store

---
+++ src/app/core/store/root-store.module.ts
@@ -0,0 +1,21 @@
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { environment } from 'src/environments/environment';
+
+@NgModule({
+  declarations: [],
+  providers: [],
+  imports: [
+    CommonModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+  ],
+})
+export class RootStoreModule {}
