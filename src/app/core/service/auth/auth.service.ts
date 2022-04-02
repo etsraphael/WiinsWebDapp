5eeb1ece9f78659eb47ccffeaecc2ac52ce80cad
etsraphael <ets_raphael@outlook.fr>
2022-04-02 11:55:50 +1000

    verify account added

--- src/app/core/service/auth/auth.service.ts
+++ src/app/core/service/auth/auth.service.ts
@@ -36,21 +36,23 @@ export class AuthService {
   }
 
   async verifyAccount(): Promise<void> {
+    const accountConnected: string = await this.getAccountConnected();
+
     return this.web3.eth.personal
       .sign(
-        'Please verify your account',
-        '0x9571A4C51Ae37976c3A6F8b0E781688b686d253a',
+        `Please sign to let us verify that you are the owner of this address ${accountConnected}`,
+        '0xb121E31149AC9E51cB159705e5a3F4b8E614B5E4',
         null!
       )
       .then(
         (response: string) => {
           console.log('response ');
-          console.log(response)
+          console.log(response);
         },
         (error: string) => {
           console.log('error: ');
-          console.log(error)
-        },
+          console.log(error);
+        }
       );
   }
 
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/service/auth/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.authService.verifyAccount();
   }
 }
