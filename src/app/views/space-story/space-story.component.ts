742749b0029aaf0eec10561626cecf872027d0cd
Nathanael421 <nathanael.rayapin@gmail.com>
2022-04-23 14:39:16 +0200

    display post in space story with store

--- src/app/views/space-story/space-story.component.ts
+++ src/app/views/space-story/space-story.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { NgxMasonryOptions } from 'ngx-masonry';
+import { Store } from '@ngrx/store';
+import { PostModel } from 'src/app/core/model/post/post.model';
 
 @Component({
   selector: 'app-space-story',
@@ -8,32 +9,16 @@ import { NgxMasonryOptions } from 'ngx-masonry';
   styleUrls: ['./space-story.component.scss']
 })
 export class SpaceStoryComponent implements OnInit {
-  // Image & Limit => Must be deleted when we have all User Data
-  masonryImages = [];
-  limit = 15;
+  publicationPosts: PostModel[] = [];
 
-  constructor(public router: Router) { }
-
-  dummyPictures = [
-    [false, 'https://source.unsplash.com/433x649/?Uruguay'],
-    [false, 'https://source.unsplash.com/530x572/?Jamaica'],
-    [false, 'https://source.unsplash.com/531x430/?Kuwait'],
-    [false, 'https://source.unsplash.com/586x1073/?Bermuda'],
-    [false, 'https://source.unsplash.com/500x571/?Ecuador'],
-    [false, 'https://source.unsplash.com/518x813/?Romania'],
-    [false, 'https://source.unsplash.com/482x1079/?Libya'],
-    [false, 'https://source.unsplash.com/473x887/?Bolivia'],
-    [false, 'https://source.unsplash.com/405x1030/?Marshall Islands'],
-    [false, 'https://source.unsplash.com/450x777/?Croatia (Hrvatska)'],
-    [false, 'https://source.unsplash.com/528x978/?Cook Islands'],
-    [false, 'https://source.unsplash.com/521x686/?Solomon Islands'],
-    [false, 'https://source.unsplash.com/550x432/?Bosnia and Herzegovina'],
-    [false, 'https://source.unsplash.com/540x631/?Tanzania'],
-    [false, 'https://source.unsplash.com/594x443/?Chad']
-  ];
+  constructor(
+    public router: Router,
+    private store: Store<{ storyPosts: { spaceStoryPosts: PostModel[] } }>) { }
 
   ngOnInit(): void {
-    this.masonryImages = this.dummyPictures.slice(0, this.limit);
+    this.store.select('storyPosts').subscribe((posts) => {
+      this.publicationPosts = posts.spaceStoryPosts;
+    })
   }
 
 }
