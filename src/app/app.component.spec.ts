8d1fe6b40bcfde19787943a40d7d8f7b5a4a9f95
etsraphael <ets_raphael@outlook.fr>
2022-04-24 22:18:00 +1000

    before to fix the last init test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,12 +19,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'WiinsWebDapp app is running!'
-    );
-  });
 });
