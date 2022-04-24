317dfcd62b0e2f5281d9b926d24f1bbdf401fcab
etsraphael <ets_raphael@outlook.fr>
2022-04-24 21:29:16 +1000

    SpaceStoryCreationPostComponent fixed

--- src/app/core/modal/space-story-creation-post/space-story-creation-post.component.spec.ts
+++ src/app/core/modal/space-story-creation-post/space-story-creation-post.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
 
 import { SpaceStoryCreationPostComponent } from './space-story-creation-post.component';
 
@@ -8,9 +12,14 @@ describe('SpaceStoryCreationPostComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SpaceStoryCreationPostComponent ]
-    })
-    .compileComponents();
+      declarations: [SpaceStoryCreationPostComponent],
+      imports: [
+        BrowserAnimationsModule,
+        FormsModule,
+        StoreModule.forRoot({})
+      ],
+      providers: [{ provide: MatDialogRef, useValue: {} }],
+    }).compileComponents();
   });
 
   beforeEach(() => {
