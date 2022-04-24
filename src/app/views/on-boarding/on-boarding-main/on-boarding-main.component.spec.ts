5f673671442d30053b75bcbb359c82802cbd233f
etsraphael <ets_raphael@outlook.fr>
2022-04-25 08:54:25 +1000

    init test 100%

--- src/app/views/on-boarding/on-boarding-main/on-boarding-main.component.spec.ts
+++ src/app/views/on-boarding/on-boarding-main/on-boarding-main.component.spec.ts
@@ -1,7 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatMenuModule } from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule } from '@ngx-translate/core';
 import { AuthMockService } from 'src/app/core/service/auth/auth-mock.service';
 import { AuthService } from 'src/app/core/service/auth/auth.service';
+import { TranslationService } from 'src/app/core/service/translate/translate.service';
 
 import { OnBoardingMainComponent } from './on-boarding-main.component';
 
@@ -11,9 +15,9 @@ describe('OnBoardingMainComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule,MatMenuModule, TranslateModule.forRoot(), BrowserAnimationsModule],
       declarations: [ OnBoardingMainComponent ],
-      providers: [{ provide: AuthService, useValue: new AuthMockService() }]
+      providers: [TranslationService, { provide: AuthService, useValue: new AuthMockService() }]
     })
     .compileComponents();
   });
