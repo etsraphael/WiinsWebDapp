8695639d75aeb9919b659e0674f438dfa9e3395a
etsraphael <ets_raphael@outlook.fr>
2022-04-25 12:35:50 +1000

    onCreatePublication clean

--- src/app/core/service/publication/feed-publication/feed-publication.service.ts
+++ src/app/core/service/publication/feed-publication/feed-publication.service.ts
@@ -1,32 +1,18 @@
 import { Injectable } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { Subscription } from 'rxjs';
 import { SpaceStoryCreationPostComponent } from 'src/app/core/modal/space-story-creation-post/space-story-creation-post.component';
-import { spaceStoryCreationPostService } from '../../angular-animation-service/feed-creation-card-animation/animation.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FeedPublicationService {
   dialogRef: MatDialogRef<SpaceStoryCreationPostComponent>;
-  dialofRefSub: Subscription;
 
-  constructor(
-    private dialog: MatDialog,
-    private creationPost: spaceStoryCreationPostService
-  ) {}
+  constructor(private dialog: MatDialog) {}
 
   onCreatePublication(): void {
     this.dialogRef = this.dialog.open(SpaceStoryCreationPostComponent, {
       panelClass: ['col-5', 'p-0'],
     });
-
-    this.dialofRefSub = this.dialogRef.afterClosed().subscribe(() => {
-      this.creationPost.toDefault();
-    });
-  }
-
-  onClosePublication(): void {
-    return this.dialofRefSub.unsubscribe();
   }
 }
