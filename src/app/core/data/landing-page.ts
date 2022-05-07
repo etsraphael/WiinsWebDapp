c291369f668828df53f571bfe7f2a177009acd4b
etsraphael <ets_raphael@outlook.fr>
2022-05-07 13:16:26 +1000

    loop is done

--- src/app/core/data/landing-page.ts
+++ src/app/core/data/landing-page.ts
@@ -19,27 +19,36 @@ export const landingPageNavData: btnLandingPageInterface[] = [
 
 export const dataSectionWithCarousel: landingPageSectionWithCarousel[] = [
   {
-    title: 'title1',
-    desc: 'lksdafdso fnsdlfnjsd kfjdskf ksdjhfk dksfjhs dfksdjhfsdkjf sdkfj',
+    title: 'LANDING_PAGE.space-news-feed-disover.title',
+    desc: 'LANDING_PAGE.space-news-feed-disover.text',
     imgList: [
       '../../../../../assets/img/landing-page/news-feed.svg',
       '../../../../../assets/img/landing-page/news-feed.svg',
     ],
   },
   {
-    title: 'title2',
-    desc: 'lksdafdso fnsdlfnjsd kfjdskf ksdjhfk dksfjhs dfksdjhfsdkjf sdkfj',
+    title: 'LANDING_PAGE.space-messenger.title',
+    desc: 'LANDING_PAGE.space-messenger.text',
     imgList: [
-      '../../../../../assets/img/landing-page/news-feed.svg',
-      '../../../../../assets/img/landing-page/news-feed.svg',
+      '../../../../../assets/img/landing-page/messenger-phone-1.svg',
+      '../../../../../assets/img/landing-page/messenger-phone-2.svg',
     ],
   },
   {
-    title: 'title3',
-    desc: 'lksdafdso fnsdlfnjsd kfjdskf ksdjhfk dksfjhs dfksdjhfsdkjf sdkfj',
+    title: 'LANDING_PAGE.space-profil.title',
+    desc: 'LANDING_PAGE.space-profil.text',
     imgList: [
-      '../../../../../assets/img/landing-page/news-feed.svg',
-      '../../../../../assets/img/landing-page/news-feed.svg',
+      '../../../../../assets/img/landing-page/profile-phone.svg',
+    ],
+  },
+  {
+    title: 'LANDING_PAGE.space-music-video.title',
+    desc: 'LANDING_PAGE.space-music-video.text',
+    imgList: [
+      '../../../../../assets/img/landing-page/phone-music-1.svg',
+      '../../../../../assets/img/landing-page/phone-music-2.svg',
+      '../../../../../assets/img/landing-page/phone-video-1.svg',
+      '../../../../../assets/img/landing-page/phone-video-2.svg',
     ],
   },
 ];
