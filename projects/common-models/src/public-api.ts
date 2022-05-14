97d8fc87c9867e220fd20c9b80bbea76e814760c
etsraphael <ets_raphael@outlook.fr>
2022-05-14 16:36:19 +1000

    code clean

--- projects/common-models/src/public-api.ts
+++ projects/common-models/src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of common-models
  */
 
-export * from './lib/common-models.service';
 export * from './lib/common-models.component';
 export * from './lib/common-models.module';
 export * from './lib/models';
