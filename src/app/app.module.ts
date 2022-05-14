8b7178f923eb09b9ca3b5134a1f5f34f5ed23148
etsraphael <ets_raphael@outlook.fr>
2022-05-14 14:36:40 +1000

    project imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,42 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
-import { MatDialogModule } from '@angular/material/dialog';
+import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { EffectsModule } from '@ngrx/effects';
-import { RootStoreModule } from './core/store';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { NgxMasonryModule } from 'ngx-masonry';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-import { FormsModule } from '@angular/forms';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-
+import { AppRoutingModule } from './app-routing.module';
+import { RootStoreModule } from './core/store';
 import { AppComponent } from './app.component';
-import { OnBoardingMainComponent } from './views/on-boarding/on-boarding-main/on-boarding-main.component';
-import { ContactUsComponent } from './views/on-boarding/contact-us/contact-us.component';
-import { MainSearchBarComponent } from './core/component/main-search-bar/main-search-bar.component';
 import { MainNavBarComponent } from './core/component/main-nav-bar/main-nav-bar.component';
-import { SpaceStoryComponent } from './views/space-story/space-story.component';
+import { MainSearchBarComponent } from './core/component/main-search-bar/main-search-bar.component';
 import { MainSidebarComponent } from './core/component/main-sidebar/main-sidebar.component';
 import { HomeLayoutComponent } from './core/layout/home-layout/home-layout.component';
 import { SettingLayoutComponent } from './core/layout/setting-layout/setting-layout.component';
 import { SpaceStoryCreationPostComponent } from './core/modal/space-story-creation-post/space-story-creation-post.component';
-
+import { ContactUsComponent } from './views/on-boarding/contact-us/contact-us.component';
+import { OnBoardingMainComponent } from './views/on-boarding/on-boarding-main/on-boarding-main.component';
+import { SpaceStoryComponent } from './views/space-story/space-story.component';
+import { IvyCarouselModule } from 'angular-responsive-carousel';
+import { NgxDropzoneModule } from 'ngx-dropzone';
 import { environment } from '../environments/environment';
-import { SpaceMusicComponent } from './views/space-music/space-music.component';
-import { SpaceTubeComponent } from './views/space-tube/space-tube.component';
-import { SpaceMessengerComponent } from './views/space-messenger/space-messenger.component';
-import { SpaceExplorerComponent } from './views/space-explorer/space-explorer.component';
 import { ComingSoonFullScreenComponent } from './core/component/coming-soon-full-screen/coming-soon-full-screen.component';
 import { FeedPublicationCardComponent } from './core/component/feed-publication-card/feed-publication-card.component';
 import { PublicationTextComponent } from './core/component/publication-text/publication-text.component';
-import { NgxDropzoneModule } from 'ngx-dropzone';
-import { IvyCarouselModule } from 'angular-responsive-carousel';
-
+import { SpaceExplorerComponent } from './views/space-explorer/space-explorer.component';
+import { SpaceMessengerComponent } from './views/space-messenger/space-messenger.component';
+import { SpaceMusicComponent } from './views/space-music/space-music.component';
+import { SpaceTubeComponent } from './views/space-tube/space-tube.component';
+import { FeedPublicationCardModule } from 'projects/feed-publication-card/src/public-api';
 @NgModule({
   declarations: [
     AppComponent,
@@ -87,6 +85,7 @@ import { IvyCarouselModule } from 'angular-responsive-carousel';
     EffectsModule.forRoot([]),
     BrowserAnimationsModule,
     NgxDropzoneModule,
+    FeedPublicationCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
