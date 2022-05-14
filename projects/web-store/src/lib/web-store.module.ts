b8f463664a6f81752f9be971c45f34dae799039e
etsraphael <ets_raphael@outlook.fr>
2022-05-14 15:39:34 +1000

    ngrx store added in a library

--- projects/web-store/src/lib/web-store.module.ts
+++ projects/web-store/src/lib/web-store.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { WebStoreComponent } from './web-store.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [WebStoreComponent],
-  imports: [],
+  providers: [{ provide: 'environment', useValue: environment }],
+  imports: [
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+    EffectsModule.forRoot([]),
+  ],
   exports: [WebStoreComponent],
 })
 export class WebStoreModule {}
