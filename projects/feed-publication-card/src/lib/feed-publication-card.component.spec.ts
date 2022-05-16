b19f64cae42c94b11d5a19599716601d149680b4
etsraphael <ets_raphael@outlook.fr>
2022-05-16 22:58:02 +1000

    feed publication is coming as a package

--- projects/feed-publication-card/src/lib/feed-publication-card.component.spec.ts
+++ projects/feed-publication-card/src/lib/feed-publication-card.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FeedPublicationCardComponent } from './feed-publication-card.component';
 
@@ -8,6 +9,7 @@ describe('FeedPublicationCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [BrowserAnimationsModule],
       declarations: [FeedPublicationCardComponent],
     }).compileComponents();
   });
--- projects/feed-publication-card/src/lib/feed-publication-card.module.ts
+++ projects/feed-publication-card/src/lib/feed-publication-card.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FeedPublicationCardComponent } from './feed-publication-card.component';
 
 @NgModule({
   declarations: [FeedPublicationCardComponent],
-  imports: [BrowserModule, CommonModule],
+  imports: [BrowserModule, CommonModule, BrowserAnimationsModule],
   exports: [FeedPublicationCardComponent],
 })
 export class FeedPublicationCardModule {}
