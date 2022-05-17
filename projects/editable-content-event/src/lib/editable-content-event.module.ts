41d2c3877d71ae7a6109711a2b030c67c8440966
etsraphael <ets_raphael@outlook.fr>
2022-05-17 21:48:08 +1000

    editable-content-event created

---
+++ projects/editable-content-event/src/lib/editable-content-event.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { EditableContentEventComponent } from './editable-content-event.component';
+
+@NgModule({
+  declarations: [EditableContentEventComponent],
+  imports: [],
+  exports: [EditableContentEventComponent],
+})
+export class EditableContentEventModule {}
---
+++ projects/editable-content-event/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of editable-content-event
+ */
+
+export * from './lib/editable-content-event.service';
+export * from './lib/editable-content-event.component';
+export * from './lib/editable-content-event.module';
