cfa84389ab8fe970dd89ad7bb9fce7ea3d198e96
Nathanael421 <nathanael.rayapin@gmail.com>
2022-05-31 13:52:52 +0200

    Deleted files

--- src/app/core/data/social-media-list.ts
+++ src/app/core/data/social-media-list.ts
@@ -3,27 +3,27 @@ export const socialMediaLists: socialLists[] = [
   {
     name: 'facebook',
     path: 'https://www.facebook.com/pages/category/Photography-Videography/Wiins-109652504066491/',
-    src: '../../../../../assets/img/core/icon/facebook.svg',
+    src: 'https://images.unsplash.com/photo-1653856289645-8601ab0e422f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
   },
   {
     name: 'instagram',
     path: 'https://www.instagram.com/wiins___/',
-    src: '../../../../../assets/img/core/icon/instagram.svg',
+    src: 'https://images.unsplash.com/photo-1653856289645-8601ab0e422f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
   },
   {
     name: 'linkedin',
     path: 'https://www.linkedin.com/company/espace-wiin-s/',
-    src: '../../../../../assets/img/core/icon/linkedin.svg',
+    src: 'https://images.unsplash.com/photo-1653856289645-8601ab0e422f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
   },
   {
     name: 'twitter',
     path: 'https://twitter.com/espace_s',
-    src: '../../../../../assets/img/core/icon/twitter.svg',
+    src: 'https://images.unsplash.com/photo-1653856289645-8601ab0e422f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
   },
   {
     name: 'github',
     path: 'https://github.com/etsraphael/WiinsWebDapp',
-    src: '../../../../../assets/img/core/icon/github.svg',
+    src: 'https://images.unsplash.com/photo-1653856289645-8601ab0e422f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
   },
 ];
 
@@ -32,12 +32,12 @@ export const socialIconsLists: socialLists[] = [
   {
     name: 'discord',
     path: 'https://discord.gg/Kpdh29U6',
-    src: '../../../assets/img/coming-soon/discord.svg',
+    src: 'https://images.unsplash.com/photo-1653856289645-8601ab0e422f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
   },
   {
     name: 'github',
     path: 'https://github.com/etsraphael/WiinsWebDapp',
-    src: '../../../../assets/img/core/icon/github.svg',
+    src: 'https://images.unsplash.com/photo-1653856289645-8601ab0e422f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
   },
 ];
 
