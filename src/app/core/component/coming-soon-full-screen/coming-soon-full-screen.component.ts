54892dc40bf29756f98c0eb2a721851f47e797d2
Nathanael421 <nathanael.rayapin@gmail.com>
2022-06-01 21:05:20 +0200

    Files deleted and some texte too

--- src/app/core/component/coming-soon-full-screen/coming-soon-full-screen.component.ts
+++ src/app/core/component/coming-soon-full-screen/coming-soon-full-screen.component.ts
@@ -8,24 +8,4 @@ import { socialIconsLists, socialLists } from '../../data/social-media-list';
 })
 export class ComingSoonFullScreenComponent {
   socialIcons: socialLists[] = socialIconsLists;
-
-  // Because of the lack of informations about this button, I put the code like this
-  monetaryIcons: socialLists[] = [
-    {
-      name: 'toDefine',
-      path: 'toDefine',
-      src: '../../../../assets/img/coming-soon/zion.svg',
-    },
-    {
-      name: 'toDefine',
-      path: 'toDefine',
-      src: '../../../../assets/img/coming-soon/gift.svg',
-    },
-  ];
-}
-
-export interface monetaryLists {
-  name: string;
-  path: string;
-  src: string;
 }
