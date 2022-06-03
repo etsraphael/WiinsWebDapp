d532ec63b59c14426cb149b92d33e27b4adf8667
etsraphael <ets_raphael@outlook.fr>
2022-06-03 22:28:48 +1000

    code clean

--- scripts/libs-publish.js
+++ scripts/libs-publish.js
@@ -1,18 +1,16 @@
 const { readdir } = require('fs-extra');
 const { pexec } = require('./utils');
 
-const publishLibs = async (dirs) => {
-    for (let dir of dirs) {
-        await pexec(`npm publish dist/${dir}`)
-        .then(() => {
-            console.log(`Published @wiins/${dir}`);
-        }, () => {
-            console.log(`Failed to publish @wiins/${dir}`);
-        });
-    }
-}
+const publishLibs = async dirs => {
+  for (let dir of dirs) {
+    await pexec(`npm publish dist/${dir}`).then(
+      () => console.log(`Published @wiins/${dir}`),
+      () => console.log(`Failed to publish @wiins/${dir}`)
+    );
+  }
+};
 
 (async () => {
-    const dirs = await readdir('./projects');
-    await publishLibs(dirs);
-})()
+  const dirs = await readdir('./projects');
+  await publishLibs(dirs);
+})();
