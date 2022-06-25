b9e930036eb3b54e19559968ac79577b31c8d11f
Nathanael421 <nathanael.rayapin@gmail.com>
2022-06-25 15:46:25 +0200

    files save on the web3 storage

--- src/app/core/data/github-page-devs.ts
+++ src/app/core/data/github-page-devs.ts
@@ -1,11 +1,13 @@
 export const githubPageDevs: wiinsweb[] = [
   {
-    avatar: '../../../../../../assets/img/landing-page/raphael.jfif',
+    avatar:
+      'https://bafkreias4yjinicsecyoh3ju7xhttyxm4iigr6t5ffwema6odxlken4foi.ipfs.dweb.link/?filename=raphael.jfif',
     name: 'Raphaël',
     path: 'https://github.com/etsraphael',
   },
   {
-    avatar: '../../../../../../assets/img/landing-page/nathanael.jpg',
+    avatar:
+      'https://bafkreigacq5j5oj3boswvtq3aaoxzrdsq3kjanynna5n23tpakfaollglm.ipfs.dweb.link/?filename=nathanael.jpg',
     name: 'Nathanaël',
     path: 'https://github.com/Nathanael421',
   },
--- src/app/core/data/social-media-list.ts
+++ src/app/core/data/social-media-list.ts
@@ -3,27 +3,27 @@ export const socialMediaLists: socialLists[] = [
   {
     name: 'facebook',
     path: 'https://www.facebook.com/pages/category/Photography-Videography/Wiins-109652504066491/',
-    src: '../../../assets/img/core/icon/facebook.svg',
+    src: 'https://bafkreie4taghi3rytz6b7krnsfqciuoic7mrcrcd7ul62sswurg3ntgobm.ipfs.dweb.link/?filename=facebook.svg',
   },
   {
     name: 'instagram',
     path: 'https://www.instagram.com/wiins___/',
-    src: '../../../assets/img/core/icon/instagram.svg',
+    src: 'https://bafkreig64uypdguztsjuzoe2s6a22yaobwcedfyneji6cak6krly3cg2m4.ipfs.dweb.link/?filename=instagram.svg',
   },
   {
     name: 'linkedin',
     path: 'https://www.linkedin.com/company/espace-wiin-s/',
-    src: '../../../assets/img/core/icon/linkedin.svg',
+    src: 'https://bafkreicwpsy7qhlkn45olluj6doebwd6a5vel426tnwzeonnt3skohoq24.ipfs.dweb.link/?filename=linkedin.svg',
   },
   {
     name: 'twitter',
     path: 'https://twitter.com/espace_s',
-    src: '../../../assets/img/core/icon/twitter.svg',
+    src: 'https://bafkreie4kvhzd3zfzyhysrliveb2dnb3o62s4b6bensonvukamupjfqv2i.ipfs.dweb.link/?filename=twitter.svg',
   },
   {
     name: 'github',
     path: 'https://github.com/etsraphael/WiinsWebDapp',
-    src: '../../../assets/img/core/icon/github.svg',
+    src: 'https://bafkreiblz7fzxvuq53dqpbzp4rshede7vaeyo3uy7inxfwj3bs6wpm5zwe.ipfs.dweb.link/?filename=github.svg',
   },
 ];
 
@@ -32,12 +32,12 @@ export const socialIconsLists: socialLists[] = [
   {
     name: 'discord',
     path: 'https://discord.gg/Kpdh29U6',
-    src: '../../../assets/img/coming-soon/discord.svg',
+    src: 'https://bafkreib76pmjrtr5khg4y3kwue5kh6nw5ykixo4uihnenuxfr6cfecue7m.ipfs.dweb.link/?filename=discord.svg',
   },
   {
     name: 'github',
     path: 'https://github.com/etsraphael/WiinsWebDapp',
-    src: '../../../assets/img/core/icon/github.svg',
+    src: 'https://bafkreiblz7fzxvuq53dqpbzp4rshede7vaeyo3uy7inxfwj3bs6wpm5zwe.ipfs.dweb.link/?filename=github.svg',
   },
 ];
 
